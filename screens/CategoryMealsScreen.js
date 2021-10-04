8fe55c3a75a8afaa5cd32eb68d47c05167e67563
Mac Rowe <m.rowe42@gmail.com>
2021-10-04 16:04:55 -0400

    Implement MealDetail button

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoryMealScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Category Meal Screen</Text>
+            <Button title="Meal Details" onPress={() => {
+                props.navigation.navigate({routeName: 'MealDetail'});
+            }}/>
         </View>
     )
 }; 
