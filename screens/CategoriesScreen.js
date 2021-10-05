b7c9f590391aa823fd49992de5b25e39070d3947
Mac Rowe <m.rowe42@gmail.com>
2021-10-05 16:53:10 -0400

    Set up grid layout and styling

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,45 @@
 import React from "react";
-import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { CATEGORIES } from "../data/dummy-data";
 
 const renderGridItem = (itemData) => {
-    return <View><Text>{itemData.item.title}</Text></View>
+    return (
+        <TouchableOpacity 
+            onPress={() => {
+                props.navigation.navigate({ routeName: 'CategoryMeals' }); 
+            }}
+        > 
+            <View style={styles.gridItem}>
+                <Text>{itemData.item.title}</Text>
+            </View>
+        </TouchableOpacity>
+    );
 };
 
 const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity  
+                style={styles.gridItem} 
+                onPress={() => {
+                    props.navigation.navigate({ routeName: 'CategoryMeals' }); 
+                }}
+            > 
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
-        <FlatList data={CATEGORIES} renderItem={} numColumns={2} />
+        <FlatList 
+            keyExtractor={(item, index) => item.id} // not necessary anymore but good for legacy code
+            data={CATEGORIES} 
+            renderItem={renderGridItem} 
+            numColumns={2} 
+        />
     )
 }; 
 
@@ -18,6 +48,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
