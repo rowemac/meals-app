62a17e49b1fa09a97845b2d4e1a4fa1194f732bc
Mac Rowe <m.rowe42@gmail.com>
2021-10-12 16:04:24 -0400

    Install react-navigation-drawer

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -5,6 +5,7 @@ import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Ionicons } from '@expo/vector-icons'; 
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer'
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
