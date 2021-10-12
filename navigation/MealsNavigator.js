1f8b3a97eff0817e1eafede2eedbd278ef900d44
Mac Rowe <m.rowe42@gmail.com>
2021-10-12 16:10:00 -0400

    Begin mapping navigation for filters screen

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -5,12 +5,13 @@ import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Ionicons } from '@expo/vector-icons'; 
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { createDrawerNavigator } from 'react-navigation-drawer'
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import FiltersScreen from '../screens/FiltersScreen'; 
 import Colors from '../constants/Colors'; 
 
 const defaultStackNavOptions = {
@@ -85,5 +86,14 @@ const defaultStackNavOptions = {
             activeTintColor: Colors.accentColor
           }
         });
+
+const FiltersNavigator = createStackNavigator({
+    Filters: FiltersScreen
+}); 
+
+const MainNavigator = createDrawerNavigator({
+    MealsFavs: MealsFavTabNavigator,
+    Filters: FiltersNavigator
+}); 
   
-  export default createAppContainer(MealsFavTabNavigator);
+  export default createAppContainer(MainNavigator);
