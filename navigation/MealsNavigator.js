859d6c31caf703dcabafc6d29e40517da70fccd9
Mac Rowe <m.rowe42@gmail.com>
2021-10-12 16:50:55 -0400

    Style drawer text

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -89,11 +89,29 @@ const defaultStackNavOptions = {
 
 const FiltersNavigator = createStackNavigator({
     Filters: FiltersScreen
-}); 
+}, {
+    // navigationOptions: {
+    //     drawerLabel: 'Filters!!!'
+    // },
+    defaultNavigationOptions: defaultStackNavOptions
+  }
+); 
 
 const MainNavigator = createDrawerNavigator({
-    MealsFavs: MealsFavTabNavigator,
+    MealsFavs: {
+        screen: MealsFavTabNavigator,
+        navigationOptions: {
+            drawerLabel: 'Meals'
+        }
+    },
     Filters: FiltersNavigator
+}, {
+    contentOptions: {
+        activeTintColor: Colors.accentColor,
+        labelStyle: {
+            fontFamily: 'open-sans-bold'
+        }
+    }
 }); 
   
   export default createAppContainer(MainNavigator);
