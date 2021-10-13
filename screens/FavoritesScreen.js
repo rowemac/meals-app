012480ba8a9b5d5550e0422d1f78f6313a3b7e31
Mac Rowe <m.rowe42@gmail.com>
2021-10-13 12:11:39 -0400

    Implement useSelector to add store

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
-import MealList from "../components/MealList";
-import { MEALS } from '../data/dummy-data'
-import HeaderButton from '../components/HeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'; 
+import { useSelector } from "react-redux";
+
+import MealList from "../components/MealList";
+import HeaderButton from '../components/HeaderButton';
+
 
 
 const FavoritesScreen = props => {
-    const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+    const favMeals = useSelector(state.meals.favoriteMeals);
+
 
     return <MealList listData={favMeals} navigation={props.navigation}/>;
 }; 
