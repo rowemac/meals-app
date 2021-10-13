17ac937912ff58717e9981ea8b00703f3bf867f5
Mac Rowe <m.rowe42@gmail.com>
2021-10-13 10:19:18 -0400

    Fix typo

--- components/DefaultText.js
+++ components/DefaultText.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet } from 'react-native';
 
 const DefaultText = props => {
     return <Text style={styles.text}>{props.children}</Text>
@@ -11,4 +11,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default DefaultText
+export default DefaultText;
