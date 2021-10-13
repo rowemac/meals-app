9c94924e7f0c2fff2e02a08f13cd70c6cd8f8118
Mac Rowe <m.rowe42@gmail.com>
2021-10-13 13:13:22 -0400

    Add action for favoriting a meal

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from "../components/DefaultText";
+import { toggleFavorite } from '../store/actions/meals'; 
 
 const ListItem = props => {
     return (
@@ -20,9 +21,16 @@ const MealDetailScreen = props => {
 
     const selectedMeal = availableMeals.find(meal => meal.id === mealId); 
 
-    // useEffect(() => {
-    //     props.navigation.setParams({mealTitle: selectedMeal.title}); 
-    // }, [selectedMeal]); ONE POSSIBLE SOLUTION BUT IT RUNS AFTER RERENDER SO THE TITLE SHOWS UP LATE
+    const dispatch = useDispatch(); 
+
+    const toggleFavoriteHandler = useCallback(() => {
+        dispatch(toggleFavorite(mealId)); 
+    }, [dispatch, mealId]);  
+
+    useEffect(() => {
+        // props.navigation.setParams({ mealTitle: selectedMeal.title }); DELAYED TITLE UPDATE BECAUSE OF RERENDER 
+        props.navigation.setParams({toggleFav: toggleFavoriteHandler}); 
+    }, [selectedMeal]);
 
     
     return (
@@ -51,8 +59,9 @@ const MealDetailScreen = props => {
 }; 
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-    const mealId = navigationData.navigation.getParam('mealId'); 
+    // const mealId = navigationData.navigation.getParam('mealId'); 
     const mealTitle = navigationData.navigation.getParam('mealTitle');
+    const toggleFavorite = navigationData.navigation.getParam('toggleFav'); 
     // const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
         headerTitle: mealTitle,
@@ -61,9 +70,7 @@ MealDetailScreen.navigationOptions = (navigationData) => {
                 <Item 
                 title="Favorite" 
                 iconName="ios-star"
-                onPress={() => {
-                    console.log('Mark as favorite!')
-                }}
+                onPress={toggleFavorite}
             />
             </HeaderButtons>
         )
