c15bfc9dce63b376aa895986e348adbae7b46aae
Mac Rowe <m.rowe42@gmail.com>
2021-10-13 12:09:01 -0400

    Implement useSelector to add store

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { CATEGORIES, MEALS } from '../data/dummy-data';
+import { useSelector } from 'react-redux'; 
+
+import { CATEGORIES } from '../data/dummy-data';
 import MealList from "../components/MealList";
 
 const CategoryMealScreen = props => {
     const catId = props.navigation.getParam('categoryId');
+
+    const availableMeals = useSelector(state => state.meals.filteredMeals); 
 
     const displayedMeals = MEALS.filter(
         meal => meal.categoryIds.indexOf(catId) >= 0
