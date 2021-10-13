1ea4757f1dcc8e177db96e97b8adaf933dce0ba9
Mac Rowe <m.rowe42@gmail.com>
2021-10-13 13:41:05 -0400

    Add a message if no favorite meals selected

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'; 
 import { useSelector } from "react-redux";
 
 import MealList from "../components/MealList";
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from "../components/DefaultText";
 
 
 
 const FavoritesScreen = props => {
     const favMeals = useSelector(state => state.meals.favoriteMeals);
 
+    if (favMeals.length === 0 || !favMeals ) {
+        return (
+            <View style={styles.emptyMessage}>
+                <DefaultText>No favorite meals yet!</DefaultText>
+            </View>
+        )
+    }
 
     return <MealList listData={favMeals} navigation={props.navigation}/>;
 }; 
@@ -29,5 +38,14 @@ FavoritesScreen.navigationOptions = (navData) => {
             </HeaderButtons>
     )}
 }; 
+
+const styles = StyleSheet.create({
+    emptyMessage: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+});
+
 
 export default FavoritesScreen; 
